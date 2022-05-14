57adb577601e874b5a3b0e780ee6428193aed25c
Julie TRAUQUE <jtrauque@e2r5p9.clusters.42paris.fr>
2022-05-14 16:02:04 +0200

    test ex03 ok

--- Module_01/ex03/generator.py
+++ Module_01/ex03/generator.py
@@ -20,3 +20,13 @@ def	generator(text, sep=" ", option=None):
 
 for word in generator("hello ca va hello toi", " "):
 	print(word)
+print("-------------------------------------------")
+for word in generator("hello ca va hello toi", " ", "unique"):
+	print(word)
+print("-------------------------------------------")
+for word in generator("a b c d e", " ", "shuffle"):
+	print(word)
+print("-------------------------------------------")
+for word in generator("b e c a d", " ", "ordered"):
+	print(word)
+print("-------------------------------------------")
