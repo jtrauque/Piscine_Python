46fca5d312408f95c802e7e9c6c5a296e10fac35
Julie TRAUQUE <jtrauque@e2r5p9.clusters.42paris.fr>
2022-05-14 17:33:35 +0200

    ex04 ok

---
+++ Module_01/ex04/eval.py
@@ -0,0 +1,18 @@
+class Evaluator:
+	@staticmethod
+	def zip_evaluate(coefs, words):
+		if len(coefs) != len(words):
+			return -1
+		ret = 0
+		for c, w in zip(coefs, words):
+			ret += (c * len(w))
+		return ret
+ 
+	@staticmethod
+	def enumerate_evaluate(coefs, words):
+		if len(coefs) != len(words):
+			return -1
+		ret = 0
+		for w in enumerate(words): # i est le compteur
+			ret += (coefs[w[0]] * len(w[1]))
+		return ret
