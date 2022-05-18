f660978fafdc1c1cb101adf644708fae7244880c
Julie TRAUQUE <jtrauque@e3r3p20.clusters.42paris.fr>
2022-05-18 14:58:51 +0200

    ex01 Module 2 ok

---
+++ Module_02/ex01/main.py
@@ -0,0 +1,38 @@
+def	what_are_the_vars(*args, **kwargs):
+	obj = ObjectC()
+	for i in enumerate(args):
+		setattr(obj, 'var_' + str(i[0]), i[1])
+	for key, value in kwargs.items():
+		setattr(obj, key, value)
+	return obj
+
+class ObjectC():
+	def __init__(self):
+		pass
+
+def doom_printer(obbj):
+	if obj is None:
+		print("ERROR")
+		print("end")
+		return
+	for attr in dir(obj):
+		if attr[0] != '_':
+			value = getattr(obj, attr)
+			print("{}:{}".format(attr, value))
+	print("end")
+
+if __name__ == "__main__":
+	obj = what_are_the_vars(7)
+	doom_printer(obj)
+	obj = what_are_the_vars(None, [])
+	doom_printer(obj)
+	obj = what_are_the_vars("ft_lol", "Hi")
+	doom_printer(obj)
+	obj = what_are_the_vars()
+	doom_printer(obj)
+	obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
+	doom_printer(obj)
+	obj = what_are_the_vars(42, a=10, var_O="world")
+	doom_printer(obj)
+	obj = what_are_the_vars(42, "Yes", a=10, var_2="world")
+	doom_printer(obj)
