7f91986a8278080dd09742e2e210bbbdd8df927b
Julie TRAUQUE <jtrauque@e3r4p15.clusters.42paris.fr>
2022-05-19 20:06:45 +0200

    still not working, check the env

--- Module_02/ex04/build.sh
+++ Module_02/ex04/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 python3 -m pip install --upgrade pip
-python3 -m pip install python3.8-venv
 python3 -m pip install --upgrade setuptools
 python3 -m pip install --upgrade wheel
 python3 -m pip install --upgrade build
@@ -9,3 +8,4 @@ python3 -m pip install --upgrade build
 python3 -m build
 
 pip install ./dist/my_minipack-1.0.0.tar.gz
+pip install ./dist/my_minipack-1.0.0-py3-none-any.whl
