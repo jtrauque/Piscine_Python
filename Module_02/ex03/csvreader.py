6ddc872df90d8bb29f58ff73e84cac366bb94649
Julie TRAUQUE <jtrauque@e2r13p8.clusters.42paris.fr>
2022-05-19 17:47:30 +0200

    M02 ex03 ok

--- Module_02/ex03/csvreader.py
+++ Module_02/ex03/csvreader.py
@@ -1,16 +1,15 @@
-#from csvreader import CsvReader 
+import os
+import sys
+sys.tracebacklimit = 0
 
 class CsvReader():
 	def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
-		if not filename:
-			raise ValueError("No Filename")
+		if not filename or not os.path.exists(filename):
+			raise ValueError("No valid filename")
 		if not sep:
 			raise ValueError("We neep separator to separate values")
-	#	try:
-	#		self.file = open(filename)
-	#	except Exception :
-	#		print("File corrupted")
-#			exit()
+		#with open(filename) as fp:
+		#	num_lines = sum(1 for line in fp)
 		num_lines = sum(1 for line in open(filename))
 		self.sep = sep
 		self.header = header
@@ -18,42 +17,59 @@ class CsvReader():
 		self.bot = num_lines - skip_bottom
 		self.data = []
 		self.size = 0
-		self.first_line = ""
+		self.first_line = []
 		self.file = filename
 
 	def __enter__(self):
 		i = 0
-		#for line in self.file.readlines():
 		with open(self.file) as file:
 			for line in file:
-				print(f"line = {line}")
-				line.replace("\n", "")
+				line = line.replace("\n", "").replace(" ", "")
 				words = line.split(self.sep)
-				print(f"word = {words}")
-			if self.size == 0:
-				self.size = len(words)
-				if self.header:
-					self.first_line = words	
-			if self.size == len(words) and i >= self.top and i < self.bot:
-				self.data.append(words)
-			else:
-				raise ValueError("File Corrupted")
-			i += 1
+				words = list(filter(bool, words))
+				if self.size == 0:
+					self.size = len(words)
+					if self.header == True:
+						self.first_line = words
+						continue
+				if self.size == len(words) and i >= self.top and i < self.bot:
+					self.data.append(words)
+				elif self.size != len(words):
+					raise ValueError("File Corrupted")
+				i += 1
 		return self
 			
 	def	__exit__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
-		#self.data.close()
-		pass
+		if os.path.exists(self.file):
+			try:
+				with open(self.file) as file:
+					print("the file is closed")
+			except IOError:
+				print("the file is still open")
+		#pass
 
 	def	getdata(self):
 		return self.data
 
 	def	getheader(self):
-		return self.first_line
+		if self.header:
+			return self.first_line
+		return None
 
+#from csvreader import CsvReader
 
 if __name__ == "__main__":
-	with CsvReader('good.csv') as file:
-		data = file.getdata()
-		header = file.getheader()
-		print(data)
+    filename = sys.argv[1]
+    with CsvReader(filename, skip_top=18, skip_bottom=0) as reader:
+        if reader == None:
+            print("File is corrupted or missing")
+        else:
+            print(reader.getheader(), end = "\n")
+            print(reader.getdata(), end = "\n\n")
+
+    with CsvReader(filename, header = True, skip_top=17, skip_bottom=0) as reader:
+        if reader == None:
+            print("File is corrupted or missing")
+        else:
+            print(reader.getheader(), end = "\n")
+            print(reader.getdata(), end = "\n\n")
