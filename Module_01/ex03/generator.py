2a38ef20a2ba591b4cafd9ecc967201a7681c674
Julie TRAUQUE <jtrauque@e3r3p16.clusters.42paris.fr>
2022-05-20 16:48:14 +0200

    clean

--- Module_01/ex03/generator.py
+++ Module_01/ex03/generator.py
@@ -11,24 +11,31 @@ def generator(text, sep=" ", option=None):
         exit()
     if not option:
         pass
-    elif not isinstance(option, str) or option not in ["shuffle", "unique", "ordered"]:
+    elif not isinstance(option, str) or\
+            option not in ["shuffle", "unique", "ordered"]:
         raise ValueError("The option is not valid")
     elif option == "shuffle":
         new = random.sample(new, len(new))
     elif option == "unique":
-        new = list(dict.fromkeys(new))
+        tmp = []
+        for i in new:
+            if i not in tmp:
+                tmp.append(i)
+        for i in tmp:
+            yield i
+        return
     elif option == "ordered":
         new.sort()
     for i in new:
         yield i
 
 
-for word in generator("This is a simple string for a basic test. Very simple.", " "):
+for word in generator("This is a simple test. Very simple.", " "):
     print(word)
 for word in generator("hello ca va hello toi", " "):
     print(word)
 print("-------------------------------------------")
-for word in generator("hello ca va hello toi", " ", "unique"):
+for word in generator("a a b b c c", " ", "unique"):
     print(word)
 print("-------------------------------------------")
 for word in generator("a b c d e", " ", "shuffle"):
@@ -37,16 +44,14 @@ print("-------------------------------------------")
 for word in generator("b e c a d", " ", "ordered"):
     print(word)
 print("++++++++++++TEST CORRECTION++++++++++++")
-for word in generator("This is a simple string for a basic test. Very simple.", " "):
+for word in generator("This is a simple test. Very simple.", " "):
     print(word)
 print("-----------")
-for word in generator("This is a simple string for a basic test. Very simple.", "."):
+for word in generator("This is a simple test. Very simple.", "."):
     print(word)
 print("-----------")
-for word in generator("This is a simple string for a basic test. Very simple.", "i"):
+for word in generator("This is a simple test. Very simple.", "i"):
     print(word)
 print("-----------")
-for word in generator("This is a simple string for a basic test. Very simple.", "si"):
+for word in generator("This is a simple test. Very simple.", "si"):
     print(word)
-
-
--- Module_01/ex04/eval.py
+++ Module_01/ex04/eval.py
@@ -1,18 +1,41 @@
 class Evaluator:
-	@staticmethod
-	def zip_evaluate(coefs, words):
-		if len(coefs) != len(words):
-			return -1
-		ret = 0
-		for c, w in zip(coefs, words):
-			ret += (c * len(w))
-		return ret
- 
-	@staticmethod
-	def enumerate_evaluate(coefs, words):
-		if len(coefs) != len(words):
-			return -1
-		ret = 0
-		for w in enumerate(words): # w[0] est le compteur et w[1] l elemt de la liste
-			ret += (coefs[w[0]] * len(w[1]))
-		return ret
+    @staticmethod
+    def zip_evaluate(coefs, words):
+        if not coefs or not words or len(coefs) != len(words):
+            return -1
+        ret = 0
+        for c, w in zip(coefs, words):
+            if not isinstance(w, str) or not isinstance(c, (int, float)):
+                print(type(w), type(c))
+                return -1
+            ret += (c * len(w))
+        return ret
+
+    @staticmethod
+    def enumerate_evaluate(coefs, words):
+        if not coefs or not words or len(coefs) != len(words):
+            return -1
+        ret = 0
+        # w[0] est le compteur et w[1] l elemt de la liste
+        for w in enumerate(words):
+            if not isinstance(w[1], str) or\
+                  not isinstance(coefs[w[0]], (int, float)):
+                return -1
+            ret += (coefs[w[0]] * len(w[1]))
+        return ret
+
+
+words = ["Le", "Lorem", "Ipsum", "est", "simple"]
+coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
+print(Evaluator.enumerate_evaluate(coefs, words))
+print(Evaluator.zip_evaluate(coefs, words))
+
+print("--------------Error Management--------------")
+print(Evaluator.enumerate_evaluate(None, None))
+print(Evaluator.enumerate_evaluate([1, 2, 3], []))
+print(Evaluator.enumerate_evaluate([1, 2, 3], ["word", 2, "wordo"]))
+
+words = ["Le", "Lorem", "Ipsum", "n’", "est", "pas", "simple"]
+coefs = [0.0, -1.0, 1.0, -12.0, 0.0, 42.42]
+print(Evaluator.enumerate_evaluate(coefs, words))
+print(Evaluator.zip_evaluate(coefs, words))
