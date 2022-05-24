821912acd3766858edb113c2a26259dbefe0e850
Julie TRAUQUE <jtrauque@e3r4p18.clusters.42paris.fr>
2022-05-24 15:51:03 +0200

    continue?

---
+++ Module_03/ex04/Kmeans.py
@@ -0,0 +1,11 @@
+from csvreader import CsvReader
+
+class KmeansClustering:
+    def __init__(self, max_iter=20, ncentroid=4):
+        self.ncentroid = ncentroid
+        self.max_iter = max_iter
+        self.centroids = []
+
+    def fit(self, X):
+
+    def predict(self, X):
